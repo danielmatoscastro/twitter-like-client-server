7cc91d43a803cf0a840eb05d1cb357039015ef1c
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2021-08-26 19:48:55 -0300

    initial commit

---
+++ client/cmake-build-debug/CMakeFiles/twitter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/twitter.dir/main.cpp.o"
+  "twitter"
+  "twitter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/twitter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ server/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I'm a server" << std::endl;
+    return 0;
+}
